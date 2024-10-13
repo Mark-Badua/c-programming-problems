af6673984c3fe50e87257787ec027017184952cb
Mark-Badua <markbadua92@gmail.com>
2024-10-13 22:03:27 +0800

    refactor: remove unused variables & handle scenario if twoSum returns NULL

--- two_sum/two_sum_brute_force.c
+++ two_sum/two_sum_brute_force.c
@@ -5,7 +5,7 @@ int* twoSum(int* nums, int numSize, int target);
 
 int main(){
 	int* output_ptr;
-	int i, num_size;
+	int i;
 	int nums_array[3][4] = {
 		{2, 7, 11, 15},
 		{3, 2, 4},
@@ -16,7 +16,17 @@ int main(){
 	};
 	for (i=0; i<3; ++i){
 		output_ptr = twoSum(&nums_array[i][0], 4, targets[i]);
+		if (output_ptr==NULL){
+			printf("No Solution found\n");
+			continue;
+		};
 		printf("indices: %d and %d\n", output_ptr[0], output_ptr[1]);
+		printf(
+			"%d+%d = %d\n",
+			nums_array[i][output_ptr[0]],
+			nums_array[i][output_ptr[1]],
+			targets[i]
+		);
 		free(output_ptr);
 	}
 	output_ptr=NULL;
@@ -25,16 +35,14 @@ int main(){
 };
 
 int* twoSum(int* nums, int numSize, int target){
-	int index1, index2;
 	int* ptr;
-	ptr = (int*)malloc(2*sizeof(int));
+	ptr = malloc(2*sizeof(int));
 	if (ptr==NULL){
 		fprintf(stderr, "Memory allocation failed\n");
 		return NULL;
 	}
-	//try removing this and declare variable inside loops
-	for (index1 = 0; index1!=numSize-1; ++index1){
-		for (index2=index1+1; index2<numSize; ++index2){
+	for (int index1 = 0; index1!=numSize-1; ++index1){
+		for (int index2=index1+1; index2<numSize; ++index2){
 			if (nums[index1]+nums[index2]==target){
 				ptr[0] = index1;
 				ptr[1] = index2;
@@ -42,5 +50,6 @@ int* twoSum(int* nums, int numSize, int target){
 			}
 		}
 	}
+	free(ptr);
 	return NULL;
 };
